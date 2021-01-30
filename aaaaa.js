34dc7aa0b07085048af35b86f6d31a6042476b45
Pham Toan <phamtoan2255@gmail.com>
2021-01-31 01:59:10 +0700

    toan di ngu

--- aaaaa.js
+++ aaaaa.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
-export default class toan1 extends Component {
+export default class aaaaa extends Component {
     render() {
         return (
             <View>
-                <Text> taon dep traiii </Text>
+                <Text> textInComponent </Text>
             </View>
         )
     }
