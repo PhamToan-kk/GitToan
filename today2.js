d75a30beb6d37fef9c0a8761d9350483f7bc6cc6
Pham Toan <phamtoan2255@gmail.com>
2021-02-04 00:30:25 +0700

    toan commit 1

--- today2.js
+++ today2.js
@@ -5,8 +5,7 @@ export default class today2 extends Component {
     render() {
         return (
             <View>
-                <Text> toan </Text>
-                
+                <Text> toan </Text>  
             </View>
         )
     }
